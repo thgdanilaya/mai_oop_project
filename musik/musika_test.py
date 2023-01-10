0c4219213451d17c53148086b5c901e80711f581
ssForz <druholskiy@gmail.com>
2023-01-10 18:49:14 +0200

    added musikka

---
+++ musik/musika_test.py
@@ -0,0 +1,21 @@
+import os
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+from parse.parse_test import parse_args
+from models import Models_functions
+from utils import Utils_functions
+
+if __name__ == "__main__":
+
+    # parse args
+    args = parse_args()
+
+    # initialize networks
+    M = Models_functions(args)
+    M.download_networks()
+    models_ls = M.get_networks()
+
+    # test musika
+    U = Utils_functions(args)
+    U.render_gradio(models_ls, train=False)
