d065aff55d48251568e620d1f95234e83d3f4341
ssForz <druholskiy@gmail.com>
2023-01-14 07:44:24 +0300

    Declare queue in consumer too

--- musik/app_consumer.py
+++ musik/app_consumer.py
@@ -27,6 +27,7 @@ amqp_url = os.environ['AMQP_URL_FROM_BOT']
 url_params = pika.URLParameters(amqp_url)
 connection = pika.BlockingConnection(url_params)
 channel = connection.channel()
+channel.queue_declare(queue='frombot')
 channel.basic_consume(queue='frombot', auto_ack=True, on_message_callback=callback)
 channel.start_consuming()
     
